a44b6c16255dddf2465c03a592cb88c6f256c88d
lastsunday <lastsunday@yeah.net>
2025-07-20 11:28:22 +0800

    ci(server): fix docker image (#2)

--- apps/server/script/download_model.sh
+++ apps/server/script/download_model.sh
@@ -10,10 +10,11 @@ cd ..
 mkdir -p tts
 cd tts || exit
 wget https://github.com/k2-fsa/sherpa-onnx/releases/download/tts-models/kokoro-multi-lang-v1_1.tar.bz2
-tar xvf kokoro-multi-lang-v1_1.tar.bz2
+tar xvfj kokoro-multi-lang-v1_1.tar.bz2
 rm -rf kokoro-multi-lang-v1_1.tar.bz2
 cd ..
 mkdir -p vad
 cd vad || exit
-wget wget https://github.com/snakers4/silero-vad/raw/refs/tags/v5.1.2/src/silero_vad/data/silero_vad.onnx
+wget https://github.com/snakers4/silero-vad/raw/refs/tags/v5.1.2/src/silero_vad/data/silero_vad.onnx
+cd ..
 cd ..
