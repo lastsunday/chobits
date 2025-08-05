c16b157a5d211df53f24dbeb9df7f431ed11f3b5
lastsunday <lastsunday@yeah.net>
2025-08-05 13:21:06 +0800

    fix(server): test llm unit filter

--- apps/server/api/tests/llm_util_test.rs
+++ apps/server/api/tests/llm_util_test.rs
@@ -1,11 +1,13 @@
 #[cfg(test)]
 mod tests {
-    use api::ws::util::llm::filter;
+    use api::ws::util::llm::{filter, filter_think};
 
     #[tokio::test]
     async fn test_llm_util_filter() {
-        let result =
-            filter("<think>\n\n</think>\n\n我是一个AI助手。\n\n请问有什么可以帮助你的吗？\n\n");
+        let result = filter_think(
+            "<think>\n\n</think>\n\n我是一个AI助手。\n\n请问有什么可以帮助你的吗？\n\n",
+        );
+        let result = filter(&result.unwrap());
         assert_eq!(
             result,
             Some("我是一个AI助手。请问有什么可以帮助你的吗？".to_string())
