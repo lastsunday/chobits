c2974eee9af0c0c555e1c747c68c8cd4d2448aa5
lastsunday <lastsunday@yeah.net>
2026-02-07 01:45:19 +0800

    test(server): add test asr

---
+++ apps/server/api/tests/asr_test.rs
@@ -0,0 +1,33 @@
+use api::{asr::AsrFactory, util::audio::pcm_decode};
+use std::path::PathBuf;
+use tracing::debug;
+use tracing_test::traced_test;
+
+#[tokio::test]
+#[traced_test]
+#[ignore]
+/// cargo test --test asr_test -- test_asr --ignored --nocapture
+async fn test_asr() {
+    let wav_file: PathBuf = [
+        env!("CARGO_MANIFEST_DIR"),
+        "resources",
+        "test",
+        "samples_jfk.wav",
+    ]
+    .iter()
+    .collect();
+    debug!("{}", wav_file.display());
+    let (pcm_data, sample_rate) = pcm_decode(wav_file).unwrap();
+    debug!(
+        "pcm_data len = {},sample_rate = {}",
+        pcm_data.len(),
+        sample_rate
+    );
+
+    AsrFactory::init().await;
+    let asr = AsrFactory::global().default();
+    let asr = asr.clone();
+    let mut asr = asr.lock().await;
+    let result = asr.transcribe(sample_rate, &pcm_data).await;
+    debug!("{:?}", result);
+}
